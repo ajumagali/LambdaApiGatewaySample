01d3c509c863ef6eade815064545e1320208a702
ajum <chemistric-05@mail.ru>
2019-10-15 16:00:42 +0100

    Provision Lambda function

--- provision/main.tf
+++ provision/main.tf
@@ -5,7 +5,6 @@ provider "aws" {
 
 resource "aws_s3_bucket" "static-web-bucket" {
   bucket = "${var.bucket-name}"
-  acl = "public-read"
   policy = <<POLICY
 {
     "Version": "2012-10-17",
@@ -29,4 +28,33 @@ resource "aws_s3_bucket_object" "static-web-content" {
   bucket = "${aws_s3_bucket.static-web-bucket.bucket}"
   key = "index.html"
   source = "../html/index.html"
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "online_translate_lambda"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "translator" {
+  filename = "../lambda.zip"
+  function_name = "index"
+  description = "AWS Lambda function to translate from English"
+  handler = "index.handler"
+  role = "${aws_iam_role.iam_for_lambda.arn}"
+  runtime = "nodejs8.10"
+  source_code_hash = "${filebase64sha256("../lambda.zip")}"
 }
