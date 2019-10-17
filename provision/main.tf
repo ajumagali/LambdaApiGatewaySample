8132cad56cc5c2495f2039d76c45a2c29789fefb
ajum <chemistric-05@mail.ru>
2019-10-17 13:31:52 +0100

    Enable CORS using Terraform module

--- provision/main.tf
+++ provision/main.tf
@@ -65,7 +65,11 @@ resource "aws_api_gateway_integration" "post-integration"{
   integration_http_method = "POST"
   type = "AWS"
   uri = "${aws_lambda_function.translator.invoke_arn}"
-  depends_on = ["aws_api_gateway_method.post-method", "aws_lambda_function.translator", "aws_lambda_permission.lambda-permission"]
+  depends_on = [
+    "aws_api_gateway_method.post-method",
+    "aws_lambda_function.translator",
+    "aws_lambda_permission.lambda-permission"
+  ]
 }
 
 resource "aws_api_gateway_integration_response" "post-integration-response" {
@@ -116,52 +120,14 @@ resource "aws_api_gateway_integration_response" "get-integration-response" {
   depends_on = ["aws_api_gateway_method_response.get-200"]
 }
 
-resource "aws_api_gateway_method" "options-method" {
-  authorization = "NONE"
-  http_method = "OPTIONS"
-  resource_id = "${aws_api_gateway_rest_api.static-web-to-lambda-api.root_resource_id}"
-  rest_api_id = "${aws_api_gateway_rest_api.static-web-to-lambda-api.id}"
-}
+module "apigateway-cors" {
+  source  = "bridgecrewio/apigateway-cors/aws"
+  version = "1.1.0"
+  # insert the 3 required variables here
+  api = aws_api_gateway_rest_api.static-web-to-lambda-api.id
+  resources = [aws_api_gateway_rest_api.static-web-to-lambda-api.root_resource_id]
 
-resource "aws_api_gateway_method_response" "options-200" {
-  http_method = "${aws_api_gateway_method.options-method.http_method}"
-  resource_id = "${aws_api_gateway_rest_api.static-web-to-lambda-api.root_resource_id}"
-  rest_api_id = "${aws_api_gateway_rest_api.static-web-to-lambda-api.id}"
-  status_code = "200"
-  response_models = {
-    "application/json" = "Empty"
-  }
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true,
-    "method.response.header.Access-Control-Allow-Methods" = true,
-    "method.response.header.Access-Control-Allow-Origin" = true
-  }
-  depends_on = ["aws_api_gateway_method.options-method"]
-}
-
-resource "aws_api_gateway_integration" "options-integration" {
-  http_method = "${aws_api_gateway_method.options-method.http_method}"
-  resource_id = "${aws_api_gateway_rest_api.static-web-to-lambda-api.root_resource_id}"
-  rest_api_id = "${aws_api_gateway_rest_api.static-web-to-lambda-api.id}"
-  integration_http_method = "POST"
-  type = "MOCK"
-  depends_on = [
-    "aws_api_gateway_method.options-method",
-    "aws_lambda_permission.lambda-permission"
-  ]
-}
-
-resource "aws_api_gateway_integration_response" "options-integration-response" {
-  rest_api_id   = "${aws_api_gateway_rest_api.static-web-to-lambda-api.id}"
-  resource_id   = "${aws_api_gateway_rest_api.static-web-to-lambda-api.root_resource_id}"
-  http_method   = "${aws_api_gateway_method.options-method.http_method}"
-  status_code   = "${aws_api_gateway_method_response.options-200.status_code}"
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
-    "method.response.header.Access-Control-Allow-Origin" = "'*'"
-  }
-  depends_on = ["aws_api_gateway_method_response.options-200"]
+  methods = ["GET", "POST"]
 }
 
 resource "aws_api_gateway_deployment" "translator-deployment" {
@@ -169,7 +135,6 @@ resource "aws_api_gateway_deployment" "translator-deployment" {
   stage_name = "test"
 
   depends_on = [
-    "aws_api_gateway_integration.options-integration",
     "aws_api_gateway_integration.post-integration",
     "aws_api_gateway_integration.get-integration"
   ]
